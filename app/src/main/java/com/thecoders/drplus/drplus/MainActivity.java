d265b2a267514d55f03f8565ffb2c3da7e80e17e
Rania <raniakrichen07@gmail.com>
2016-11-27 02:32:59 +0100

    Button1 works

--- app/src/main/java/com/thecoders/drplus/drplus/MainActivity.java
+++ app/src/main/java/com/thecoders/drplus/drplus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.thecoders.drplus.drplus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +9,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.thecoders.drplus.drplus.Models.Main2Activity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2;
+    EditText t1,t2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
+        b1=(Button)findViewById(R.id.button);
+        b2=(Button)findViewById(R.id.button2);
 
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
             }
-        });*/
+        });
+
+
     }
 
     @Override
