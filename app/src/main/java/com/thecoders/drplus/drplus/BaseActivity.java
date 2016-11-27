fc753d03d677bc623eb261e6d10cbc2eb2715697
Rania <raniakrichen07@gmail.com>
2016-11-27 07:55:28 +0100

    base

--- app/src/main/java/com/thecoders/drplus/drplus/BaseActivity.java
+++ app/src/main/java/com/thecoders/drplus/drplus/BaseActivity.java
@@ -1,9 +1,12 @@
 package com.thecoders.drplus.drplus;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class BaseActivity extends AppCompatActivity {
 
-
 }
+
