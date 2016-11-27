181a08d2c87072b02bb408d6bd93997f2cda13e4
Rania <raniakrichen07@gmail.com>
2016-11-27 04:48:30 +0100

    changement

--- app/src/main/java/com/thecoders/drplus/drplus/HomeActivity.java
+++ app/src/main/java/com/thecoders/drplus/drplus/HomeActivity.java
@@ -3,6 +3,8 @@ package com.thecoders.drplus.drplus;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
